956ba638d48fc391ee37903f52645e3286b8b2cd
Hiruy Legesse Adane <hiruyadane@gmail.com>
2025-11-02 22:43:25 +0300

    Time: 11 ms (83.41%) | Memory: 20.6 MB (75.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,9 +3,8 @@ class Solution:
         anagrams = defaultdict(list)
         
         for word in strs:
-            # Sort the word to use as a key
             sorted_word = ''.join(sorted(word))
             anagrams[sorted_word].append(word)
         
-        # Return grouped anagrams
+
         return list(anagrams.values())

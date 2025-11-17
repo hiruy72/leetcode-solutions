89d8e6a21c887ffbc933848f508c30bacf0e9106
Hiruy Legesse Adane <hiruyadane@gmail.com>
2025-11-17 23:49:57 +0300

    Time: 0 ms (100.00%) | Memory: 61.5 MB (48.06%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int lastOneIndex = -1; // stores index of the previous 1
+        int lastOneIndex = -1; 
 
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 1) {
                 if (lastOneIndex != -1 && i - lastOneIndex - 1 < k) {
-                    return false; // distance between 1s is less than k
+                    return false; 
                 }
-                lastOneIndex = i; // update last 1 index
+                lastOneIndex = i; 
             }
         }
 
-        return true; // all 1s are at least k apart
+        return true; 
     }
 };
